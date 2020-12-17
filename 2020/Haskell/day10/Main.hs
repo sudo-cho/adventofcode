19921901c21b17b7b49737fcbc53b97117d59987
sudo-cho <noisiv@outlook.fr>
2020-12-17 22:16:22 +0100

    day 10 complete

--- 2020/Haskell/day10/Main.hs
+++ 2020/Haskell/day10/Main.hs
@@ -2,14 +2,15 @@ import Data.List
 
 main :: IO ()
 main = do
-  contents <- readFile "input"
-  print . part1 . map readInt . lines $ contents
+  input <- sort . map readInt . lines <$> readFile "input"
+  print $ part1 input
+  print $ part2 input [(0, 1)]
 
 readInt :: String -> Int
 readInt = read
 
 part1 :: [Int] -> Int
-part1 = result . getJolt . sort . prependMyAdapter
+part1 = result . getJolt . prependMyAdapter
 
 prependMyAdapter :: [Int] -> [Int]
 prependMyAdapter x = 0 : (maximum x) + 3 : x
@@ -22,3 +23,13 @@ result xs = ones * threes
   where
     ones = length $ filter (\x -> x == 1) xs
     threes = length $ filter (\x -> x == 3) xs
+
+-- You don't need tribonacci or smthg if you can memoize
+-- the results of the recursion
+part2 :: [Int] -> [(Int, Int)] -> Int
+part2 (x:xs) memo =
+  case xs of
+    [] -> i
+    _  -> part2 xs ((x, i):memo)
+  where
+    i = sum . map snd $ takeWhile (\(y, _) -> x - y <= 3) memo
