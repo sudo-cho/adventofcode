41d5a9c07daf8f995c15b1cce6899d40ba3e2065
sudo-cho <noisiv@outlook.fr>
2021-12-01 18:08:36 +0100

    refactor using function and zip

--- 2021/rust/src/01/01_sonar_sweep.rs
+++ 2021/rust/src/01/01_sonar_sweep.rs
@@ -1,28 +1,20 @@
 use std::fs;
 
+fn result(l: &[i32], skip: usize) -> usize {
+    l.iter()
+     .zip(l.iter().skip(skip))
+     .filter(|(a, b)| b > a)
+     .count()
+}
+
 pub fn run () {
     let data = fs::read_to_string("./src/01/input.txt").expect("Unable to read file");
 
-    let lines = data
+    let lines: Vec<i32> = data
         .lines()
         .map(|s| s.parse().unwrap())
-        .collect::<Vec<i32>>();
+        .collect();
 
-    // windows allow to group value by tuple
-    let result1 = &lines
-        .windows(2)
-        .filter(|n| n[0] < n[1])
-        .count();
-
-    let result2 = &lines
-        .windows(3)
-        .map(|w| w.iter().sum())
-        // could be refactor as function to remove dup
-        .collect::<Vec<i32>>()
-        .windows(2)
-        .filter(|n| n[0] < n[1])
-        .count();
-
-    println!("{:#?}", result1);
-    println!("{:#?}", result2);
+    println!("Part 1: {:#?}", result(&lines, 1));
+    println!("Part 2: {:#?}", result(&lines, 3));
 }
