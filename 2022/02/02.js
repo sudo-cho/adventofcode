54b2c0eef88c23051e5510d8878667bef3e523bb
noise IV <noiseiv@pm.me>
2022-12-05 14:22:44 +0100

    clean ex2

--- 2022/02/02.js
+++ 2022/02/02.js
@@ -8,42 +8,26 @@ const input = fs
 
 const enumType = { X: 1, Y: 2, Z: 3 }
 
-const enumScorePart1 = [
-  { type: 'AX', score: 3},
-  { type: 'AY', score: 6},
-  { type: 'AZ', score: 0},
-
-  { type: 'BX', score: 0},
-  { type: 'BY', score: 3},
-  { type: 'BZ', score: 6},
-
-  { type: 'CX', score: 6},
-  { type: 'CY', score: 0},
-  { type: 'CZ', score: 3}
+const enumScore = [
+  { type: 'AX', score: 3, score2: 3 },
+  { type: 'AY', score: 6, score2: 4 },
+  { type: 'AZ', score: 0, score2: 8 },
+  { type: 'BX', score: 0, score2: 1 },
+  { type: 'BY', score: 3, score2: 5 },
+  { type: 'BZ', score: 6, score2: 9 },
+  { type: 'CX', score: 6, score2: 2 },
+  { type: 'CY', score: 0, score2: 6 },
+  { type: 'CZ', score: 3, score2: 7 }
 ]
 
-const enumScorePart2 = [
-  { type: 'A X', score: 3 },
-  { type: 'A Y', score: 4 },
-  { type: 'A Z', score: 8 },
-
-  { type: 'B X', score: 1 },
-  { type: 'B Y', score: 5 },
-  { type: 'B Z', score: 9 },
-
-  { type: 'C X', score: 2 },
-  { type: 'C Y', score: 6 },
-  { type: 'C Z', score: 7 }
-]
-
-const resultArray = input.map(d => d.split(' ')).map(
-  e => enumScorePart1.find(
+const p1 = input.map(d => d.split(' ')).map(
+  e => enumScore.find(
     f => f.type === `${e[0]}${e[1]}`
   )?.score + enumType[e[1]]
 ).reduce((a, b) => a + b, 0)
 
-const resultArray2 = input.map(
-  e => enumScorePart2.find(f => f.type === e)?.score
+const p2 = input.map(
+  e => enumScore.find(f => f.type === e.replace(/\s/, ''))?.score2
 ).reduce((a, b) => a + b, 0)
 
-console.log(resultArray, resultArray2)
+console.log(p1, p2)
